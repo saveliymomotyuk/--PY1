00ac16d8ee783254920ae741f63d6cc74e8bb093
saveliymomotyuk <115483143+saveliymomotyuk@users.noreply.github.com>
2022-11-02 19:34:00 +0300

    Update task4.py

--- лабораторная работа 3/task4.py
+++ лабораторная работа 3/task4.py
@@ -3,13 +3,12 @@ spend = 6000  # траты
 months = 10  # количество месяцев
 increase = 0.03  # рост цен
 
-need_money = 0  # количество денег, чтобы прожить 10 месяцев
+need = 0  # количество денег, чтобы прожить 10 месяцев
 
-grow = increase + 1
-need_money = 0  # количество денег, чтобы прожить 10 месяцев
+moneygrow = increase + 1
 for money in range(1, months +1):
     money = spend - salary
-    spend = spend * grow
-    need_money += money
+    spend = spend * moneygrow
+    need += money
 
-print(round(need_money))
+print(round(need))
