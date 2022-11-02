92779906f1bf9c3bbf37e2a3ec428ea9f06867c1
saveliymomotyuk <115483143+saveliymomotyuk@users.noreply.github.com>
2022-11-02 19:34:47 +0300

    Update task2.py

--- лабораторная работа 3/task2.py
+++ лабораторная работа 3/task2.py
@@ -1,11 +1,14 @@
 list_numbers = [2, -93, -2, 8, -36, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
 
-max_index = 0
-for i in range(len(list_numbers)):
-    max_element = list_numbers[max_index]
-    current_ = list_numbers[i]
-    if current_ > max_element:
-        max_index = i
+max_ = 0
+for number in range(len(list_numbers)):
+    numbermax = list_numbers[max_]
+    anynumber = list_numbers[number]
+    if anynumber > numbermax:
+        max_ = number
 
-list_numbers[max_index], list_numbers[-1] = list_numbers[-1], list_numbers[max_index]
+
+
+list_numbers[max_], list_numbers[-1], = list_numbers[-1], list_numbers[max_]
 print(list_numbers)
+
